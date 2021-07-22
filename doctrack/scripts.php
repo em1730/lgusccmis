adbc3b96a6667e6c009780fd6b37ec6f169037e1
em1730 <62753224+em1730@users.noreply.github.com>
2021-07-22 13:54:37 +0800

    fixed

--- doctrack/scripts.php
+++ doctrack/scripts.php
@@ -24,26 +24,7 @@
 
 
 
-  <?php
-
-  if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
-
-  ?>
-    <script>
-      swal({
-        title: "<?php echo $_SESSION['status'] ?>",
-        // text: "You clicked the button!",
-        icon: "<?php echo $_SESSION['status_code'] ?>",
-        button: "OK. Done!",
-      });
-    </script>
-
-  <?php
-    unset($_SESSION['status']);
-  }
-  ?>
-
-
+ 
 
   <script>
     $('.select2').select2();
