9497dc20f9015471e751a975d02360f388c63848
em1730 <62753224+em1730@users.noreply.github.com>
2021-07-22 10:51:19 +0800

    Update footer.php

--- doctrack/footer.php
+++ doctrack/footer.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-block">
-    <b>Version</b> 3.0.0-alpha
-  </div>
-  All rights reserved.
+    <strong>Copyright &copy; 2019 ITCSO | <a href="http://lguscc.gov.ph">Local Government of San Carlos City</a>.</strong>
+	
+    <div class="float-right d-none d-sm-inline-block">
+        All rights reserved.
+	</div>
 </footer>
