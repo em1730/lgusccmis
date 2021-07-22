3060a21701745bcef16eab5bf21d2428524ab3c0
em1730 <62753224+em1730@users.noreply.github.com>
2021-07-22 10:55:32 +0800

    Update scripts.php

--- doctrack/scripts.php
+++ doctrack/scripts.php
@@ -19,3 +19,32 @@
   <script src="../plugins/datatables/jquery.dataTables.js"></script>
   <script src="../plugins/datatables/dataTables.bootstrap4.js"></script>
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script src="../plugins/sweetalert/sweetalert.min.js"></script>
+  <script src="../plugins/select2/select2.full.min.js"></script>
+
+
+
+  <?php
+
+  if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+
+  ?>
+    <script>
+      swal({
+        title: "<?php echo $_SESSION['status'] ?>",
+        // text: "You clicked the button!",
+        icon: "<?php echo $_SESSION['status_code'] ?>",
+        button: "OK. Done!",
+      });
+    </script>
+
+  <?php
+    unset($_SESSION['status']);
+  }
+  ?>
+
+
+
+  <script>
+    $('.select2').select2();
+  </script>
