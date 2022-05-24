637b6c2885cfc757a40d1296d320d078c44cf8f6
itsemelona <62753224+em1730@users.noreply.github.com>
2022-05-24 18:44:20 +0800

    Update list_technician.php

--- hms/list_technician.php
+++ hms/list_technician.php
@@ -5,14 +5,14 @@ session_start();
 
 
 
+
 include('../config/db_config.php');
 
-if (!isset($_SESSION['id'])) {
-    header('location:../index');
-}
 
 
-include('sql_querries.php');
+
+
+// include('sql_querries.php');
 
 
 $user_id = $_SESSION['id'];
@@ -54,25 +54,8 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title> HM SYSTEM | List of Technician</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../plugins/font-awesome/css/font-awesome.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="../plugins/iCheck/flat/blue.css">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="../plugins/datepicker/datepicker3.css">
-    <!-- Select2 -->
-    <link rel="stylesheet" href="../plugins/select2/select2.min.css">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap4.css">
-    <!-- Bootstrap  -->
-    <link href="../plugin/bootstrap-dialog.css" rel="stylesheet" type="text/css" />
-    <!--  -->
-    <script src="js/bootstrap-dialog.js"></script>
+    <title> HMS | List of Technician</title>
+    <?php include('heading.php') ?>
 
 </head>
 
@@ -81,14 +64,17 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
     <div class="wrapper">
 
         <?php include('sidebar.php'); ?>
+
         <div class="content-wrapper">
             <div class="content-header"></div>
+
             <section class="content">
-                <div class="card card-dark">
+
+                <div class="card card-danger">
                     <div class="card-header">
                         <h4>List of Technician
 
-                            <button type="button" style="float:right;" class="btn btn-dark bg-gradient-dark" data-toggle="modal" data-target="#AddModal">
+                            <button type="button" style="float:right;" class="btn btn-danger bg-gradient-danger" data-toggle="modal" data-target="#AddModal">
                                 <i class="nav-icon fa fa-plus-square"></i>
                             </button>
                         </h4>
@@ -101,35 +87,15 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
                                         <thead align="center">
                                             <tr>
                                                 <th>ID No.</th>
-                                                <th>Full Name</th>
+                                                <th>First Name</th>
+                                                <th>Middle Name</th>
+                                                <th>Lastname </th>
+                                                <th>Position</th>
                                                 <th>Status</th>
                                                 <th>Options</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <?php while ($tech_data = $get_all_data->fetch(PDO::FETCH_ASSOC)) { ?>
-                                                <tr align="center">
-                                                    <!-- <td><input type="checkbox" value ="" name="" /> -->
-
-                                                    <td><?php echo $tech_data['idno']; ?></td>
-                                                    <td><?php echo $tech_data['firstname'];
-                                                        echo " ";
-                                                        echo $tech_data['middlename'];
-                                                        echo " ";
-                                                        echo $tech_data['lastname']; ?></td>
-                                                    <td><?php echo $tech_data['status']; ?></td>
-
-                                                    <td>
-                                                        <a class="btn btn-dark btn-sm" href="view_technician.php?objid=<?php echo $tech_data['objid']; ?>&id=<?php echo $tech_data['idno']; ?> ">
-                                                            <i class="fa fa-eye"></i>
-                                                        </a>
-                                                        <!-- <button class="btn btn-danger btn-sm" data-role="confirm_delete" data-id="<?php echo $tech_data["idno"]; ?>"><i class="fa fa-trash-o"></i>
-                                                        </button> -->
-
-                                                    </td>
-                                                </tr>
-                                            <?php } ?>
-
 
 
                                         </tbody>
@@ -150,32 +116,34 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
 
 
 
-
+    <!-- add technician info modal -->
     <div class="modal fade" id="AddModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header  bg-dark">
+                <div class="modal-header  bg-danger">
                     <h5 class="modal-title" id="exampleModalLabel "> Add Technician</h5>
-                    <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button> -->
+
                 </div>
 
-                <form method="POST" action="">
+                <form method="POST" action="query_technician.php">
                     <div class="modal-body">
 
                         <?php echo $alert_msg; ?>
                         <div class=" form-group">
                             <label for="recipient-name" class="col-form-label">First Name:</label>
-                            <input type="text" class="form-control " name="firstname" value="<?php echo $firstname; ?>" required>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="firstname">
                         </div>
                         <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Middle Name:</label>
-                            <input type="text" class="form-control " name="middlename" value="<?php echo $middlename; ?>" required>
+                            <label for="recipient-name" class="col-form-label">Middle Initial:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="middlename">
                         </div>
                         <div class="form-group">
                             <label for="recipient-name" class="col-form-label">Last Name:</label>
-                            <input type="text" class="form-control " name="lastname" value="<?php echo $lastname; ?>" required>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Position:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="position">
                         </div>
 
                         <div class="modal-footer">
@@ -192,49 +160,153 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
         </div>
     </div>
 
+    <!-- update technician info modal -->
+    <div class="modal fade" id="UpdateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header  bg-danger">
+                    <h5 class="modal-title" id="exampleModalLabel "> Update Technician</h5>
+
+                </div>
+
+                <form method="POST" action="query_technician.php">
+                    <div class="modal-body">
+
+                        <div class=" form-group">
+                            <label for="recipient-name" class="col-form-label">ID Number:</label>
+                            <input type="text" readonly class="form-control " style="text-transform: uppercase;" name="idno" id="idno">
+                        </div>
+                        <div class=" form-group">
+                            <label for="recipient-name" class="col-form-label">First Name:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="firstname" id="firstname">
+                        </div>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Middle Initial:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="middlename" id="middlename">
+                        </div>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Last Name:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="lastname" id="lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Position:</label>
+                            <input type="text" class="form-control " style="text-transform: uppercase;" name="position" id="position">
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" name="update_technician" id="btnSave" class="btn btn-success">
+                                <i class="fa fa-check fa-fw"> </i> </button>
+                            <button type="button" name="cancel" class="btn btn-danger" data-dismiss="modal">
+                                <i class="fa fa-close fa-fw"> </i> </button>
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 
 
 
-    <!-- ./wrapper -->
-    <!-- jQuery -->
-    <script src="../plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <!-- <script src="../dist/css/jquery-ui.min.js"></script> -->
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
+    <div class="modal fade" id="DeleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header  bg-danger">
+                    <h5 class="modal-title" id="exampleModalLabel "> Delete this technician?</h5>
+
+                </div>
+
+                <form method="POST" action="query_technician.php">
+                    <div class="modal-body">
+
+                        <div class=" form-group">
+                            <label for="recipient-name" class="col-form-label">ID Number:</label>
+                            <input type="text" readonly class="form-control " style="text-transform: uppercase;" name="del_idno" id="del_idno">
+                        </div>
+                        <div class=" form-group">
+                            <label for="recipient-name" class="col-form-label">Full Name:</label>
+                            <input type="text"  class="form-control " style="text-transform: uppercase;" name="del_fullname" id="del_fullname">
+                        </div>
+                     
+
+                        <div class="modal-footer">
+                            <button type="submit" name="delete_technician" id="btnSave" class="btn btn-success"> Yes
+                                <!-- <i class="fa fa-check fa-fw"> </i>  -->
+                            </button>
+                            <button type="button" name="cancel" class="btn btn-danger" data-dismiss="modal">No
+                                <!-- <i class="fa fa-close fa-fw"> </i>  -->
+                            </button>
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+
+    <?php include('scripts.php') ?>
+
+    <?php
+
+    if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
+
+    ?>
+        <script>
+            swal({
+                title: "<?php echo $_SESSION['status'] ?>",
+                // text: "You clicked the button!",
+                icon: "<?php echo $_SESSION['status_code'] ?>",
+                button: "OK. Done!",
+            });
+        </script>
+
+    <?php
+        unset($_SESSION['status']);
+    }
+    ?>
+
+
     <script>
-        // $.widget.bridge('uibutton', $.ui.button)
-    </script>
-    <!-- Bootstrap 4 -->
-    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- datepicker -->
-    <script src="../plugins/datepicker/bootstrap-datepicker.js"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-    <!-- Slimscroll -->
-    <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
-    <!-- FastClick -->
-    <script src="../plugins/fastclick/fastclick.js"></script>
-    <!-- AdminLTE App -->
-    <script src="../dist/js/adminlte.js"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="../dist/js/pages/dashboard.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../dist/js/demo.js"></script>
-    <!-- DataTables -->
-    <script src="../plugins/datatables/jquery.dataTables.js"></script>
-    <script src="../plugins/datatables/dataTables.bootstrap4.js"></script>
-    <!-- Select2 -->
+        var dataTable = $('#users').DataTable({
 
-    <script src="../plugins/select2/select2.full.min.js"></script>
-    <script>
-        $('#users').DataTable({
-            'paging': true,
-            'lengthChange': true,
-            'searching': true,
-            'ordering': false,
-            'info': true,
-            'autoWidth': true
+            page: true,
+            // searching: true,
+            stateSave: true,
+            processing: true,
+            serverSide: true,
+            scrollX: false,
+
+            ajax: {
+                url: "search_technician.php",
+                type: "post",
+                error: function(xhr, b, c) {
+                    console.log(
+                        "xhr=" +
+                        xhr.responseText +
+                        " b=" +
+                        b.responseText +
+                        " c=" +
+                        c.responseText
+                    );
+                }
+            },
+            columnDefs: [{
+                    width: "100px",
+                    targets: -1,
+                    data: null,
+                    defaultContent: '<a class="btn btn-success btn-sm"  id="update_modal" data-placement="top" title="Update Technician"> <i style="color:white; "class="nav-icon fa fa-edit fa-sm"></i></a>'+ '&nbsp;&nbsp;'+
+                    '<a class="btn btn-danger btn-sm"  id="delete_modal" data-placement="top" title="Delete Technician"> <i style="color:white; "class="nav-icon fa fa-trash fa-sm"></i></a>',
+                },
+
+            ],
         });
+
+
 
 
         $(document).on('click', 'button[data-role=confirm_delete]', function(event) {
@@ -244,6 +316,46 @@ $alert_msg = $btnEdit = $btnSave = $firstname = $middlename = $lastname = ' ';
 
             $('#user_id').val(user_id);
             $('#delete_PUMl').modal('toggle');
+
+        });
+
+
+        $("#users tbody").on("click", "#update_modal", function() {
+            event.preventDefault();
+            var currow = $(this).closest("tr");
+
+            var idno = currow.find("td:eq(0)").text();
+            var fname = currow.find("td:eq(1)").text();
+            var mname = currow.find("td:eq(2)").text();
+            var lname = currow.find("td:eq(3)").text();
+            var pos = currow.find("td:eq(4)").text();
+
+
+            $('#UpdateModal').modal('show');
+            $('#idno').val(idno);
+            $('#firstname').val(fname);
+            $('#middlename').val(mname);
+            $('#lastname').val(lname);
+            $('#position').val(pos);
+
+        });
+
+        $("#users tbody").on("click", "#delete_modal", function() {
+            event.preventDefault();
+            var currow = $(this).closest("tr");
+
+            var idno = currow.find("td:eq(0)").text();
+            var fname = currow.find("td:eq(1)").text();
+            var mname = currow.find("td:eq(2)").text();
+            var lname = currow.find("td:eq(3)").text();
+            // var pos = currow.find("td:eq(4)").text();
+            
+            // var fname = currow.find("td:eq(1)").text();
+
+            $('#DeleteModal').modal('show');
+            $('#del_idno').val(idno);
+            $('#del_fullname').val(fname + " " + mname + ". "+ lname);
+
 
         });
     </script>
