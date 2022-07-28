4d21ee46afeef7f494699ce06ebc17084e824d4a
itsemelona <62753224+em1730@users.noreply.github.com>
2022-07-28 15:29:07 +0800

    Update aside.php

--- include/aside.php
+++ include/aside.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="../dist/img/scclogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: ">
+          >
       <span class="brand-text font-weight-light">LGUSSC | DTS</span>
     </a>
 
